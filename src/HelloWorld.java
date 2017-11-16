55559da8cf6aa74ef5e14bdca6d0626f5bc769fd
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-16 15:41:34 -0600

    Added Syntax, Types, and Variables exercises.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,30 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World!");
+
+        int myFavoriteNumber = 4;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "All shall know and fear the name Burgerfinger Chickenstorm!";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
     }
 }

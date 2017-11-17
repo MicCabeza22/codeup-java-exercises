3ab4afe20a9eceb1357cd77b729f35bb4dba16e8
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-17 16:22:15 -0600

    Completed Display a Table of Powers exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,19 +4,30 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int userInt;
+        String choice;
 
-        System.out.print("What number would you like to go up to? ");
-        userInt = scan.nextInt();
+        do {
+            System.out.print("What number would you like to go up to? ");
+            userInt = scan.nextInt();
 
-        System.out.println("Here is a table of numbers and their squares and cubes:");
-        System.out.println("Number | Squared | Cubed");
-        System.out.println("------ | ------- | -----");
+            System.out.println("Here is a table of numbers and their squares and cubes:");
+            System.out.println("Number | Squared | Cubed");
+            System.out.println("------ | ------- | -----");
 
-        for (int i = 1; i <= userInt; i++) {
-            System.out.println(i + "      | "
-                    + (int) Math.pow(i, 2)
-                    + "       | "
-                    + (int) Math.pow(i, 3));
-        }
+            for (int i = 1; i <= userInt; i++) {
+                System.out.println(i + "      | "
+                        + (int) Math.pow(i, 2)
+                        + "       | "
+                        + (int) Math.pow(i, 3));
+            }
+
+            System.out.println("Would you like to continue? (YES/NO)");
+            choice = scan.next();
+
+            while (!choice.equals("YES") && !choice.equals("NO")) {
+                System.out.println("Would you like to continue? (YES/NO)");
+                choice = scan.next();
+            }
+        } while (choice.equals("YES"));
     }
 }

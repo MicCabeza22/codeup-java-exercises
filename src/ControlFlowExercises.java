de58266b3e61d2a01f8e91eb2025e06e10ce276b
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-17 15:00:00 -0600

    Added Loop Basics exercise 1b.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+
+        i = 2;
+        do {
+            System.out.println(i);
+            i = (int) Math.pow(i, 2);
+        } while (i < 1000000);
+    }
 }

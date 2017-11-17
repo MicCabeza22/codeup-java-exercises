3b73135be78168db01468b516504195b81eb4d5b
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-17 15:47:23 -0600

    Added Display a Table of Powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        Scanner scan = new Scanner(System.in);
+        int userInt;
+
+        System.out.print("What number would you like to go up to? ");
+        userInt = scan.nextInt();
+
+        System.out.println("Here is a table of numbers and their squares and cubes:");
+        System.out.println("Number | Squared | Cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int i = 1; i <= userInt; i++) {
+            System.out.println(i + "      | "
+                    + (int) Math.pow(i, 2)
+                    + "       | "
+                    + (int) Math.pow(i, 3));
         }
     }
 }

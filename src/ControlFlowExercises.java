689d890e7797afd52755215701eca2c9a2ef9fb3
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-17 15:09:51 -0600

    Added Control Flow exercise 1c.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,21 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
-            i++;
         }
 
-        i = 2;
-        do {
+//        i = 2;
+//        do {
+//            System.out.println(i);
+//            i = (int) Math.pow(i, 2);
+//        } while (i < 1000000);
+        for (int i = 2; i < 1000000; i = (int) Math.pow(i, 2)) {
             System.out.println(i);
-            i = (int) Math.pow(i, 2);
-        } while (i < 1000000);
+        }
     }
 }

9ee14c4f3ae828f924db23021cf376cfa16eb87f
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-17 15:23:17 -0600

    Added Fizzbuzz exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        int i = 5;
-//        while (i <= 15) {
-//            System.out.println(i);
-//            i++;
-//        }
-        for (int i = 5; i <= 15; i++) {
-            System.out.println(i);
-        }
-
-//        i = 2;
-//        do {
-//            System.out.println(i);
-//            i = (int) Math.pow(i, 2);
-//        } while (i < 1000000);
-        for (int i = 2; i < 1000000; i = (int) Math.pow(i, 2)) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }

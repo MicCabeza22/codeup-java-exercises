8709727d8c3e719048a8dcd8df583401c5c61702
MicCabeza22 <cabeza.michael@yahoo.com>
2017-11-29 16:06:08 -0600

    Recommitting due to lost data. Cause unknown.

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,37 @@
 package shapes;
 
 public class Rectangle {
+    protected int length;
+    protected int width;
+
+    public Rectangle() {}
+
+    public Rectangle(int len, int wid) {
+        length = len;
+        width = wid;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimeter() {
+        return (2 * length) + (2 * width);
+    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,19 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle {
+    protected int side;
+
+    public Square(int side) {
+        super.setLength(side);
+        super.setWidth(side);
+        this.side = side;
+    }
+
+    public int getArea() {
+        return (int) Math.pow(side, 2);
+    }
+
+    public int getPerimeter() {
+        return side * 4;
+    }
 }
